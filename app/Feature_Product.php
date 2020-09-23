df20f26b2dc300836f875b8fdfaff08a28bd1b99
Giang <jun154607@gmail.com>
2020-09-23 09:59:33 +0700

    ##

--- app/Feature_Product.php
+++ app/Feature_Product.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Feature_Product extends Model
 {
     protected $table = 'feature_products';
+    protected $fillable = ['product_name', 'description','image'];
 }
