c55f36d0f6e50d93f775b6ebb34f332b6b7edc74
Giang <jun154607@gmail.com>
2020-09-30 11:58:19 +0700

    ##

--- resources/views/menu_detail.blade.php
+++ resources/views/menu_detail.blade.php
@@ -91,30 +91,6 @@
                         </div>
                     </div>
                 </section>
-                <section class="product-related padding-bottom-100">
-                    <div class="swin-sc swin-sc-title style-2">
-                        <p class="title"><span>You May Also Like</span></p>
-                    </div>
-                    <div class="swin-sc swin-sc-product products-02 carousel-01 woocommerce">
-                        <div class="products nav-slider">
-                            @foreach($mightAlsoLike as $product)
-                            <div class="blog-item item swin-transition">
-                                <div class="block-img"><img src="assets/images/product/product-2a.jpg" alt="" class="img img-responsive">
-                                    <div class="block-circle price-wrapper"><span class="price woocommerce-Price-amount amount"><span class="price-symbol">$</span>25.0</span></div>
-                                    <div class="group-btn"><a href="javascript:void(0)" class="swin-btn btn-link"><i class="icons fa fa-link"></i></a><a href="javascript:void(0)" class="swin-btn btn-add-to-card"><i class="fa fa-shopping-basket"></i></a></div>
-                                </div>
-                                <div class="block-content">
-                                    <h5 class="title"><a href="javascript:void(0)">{{$product->product_name}}</a></h5>
-                                    <div class="info">
-                                        <div class="author">Chef <span class="name">Don Joe</span></div>
-                                        <div class="star-rating"><span style="width:80%" class="rating"><strong class="rating"></strong></span></div>
-                                    </div>
-                                </div>
-                            </div>
-                            @endforeach
-                        </div>
-                    </div>
-                </section>
             </div>
         </div>
     </div>
